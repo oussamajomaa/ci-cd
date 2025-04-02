fdc1aacda49addcd06dcfe0619f0815a7bc69512
oussamajomaa <osm70@gmx.com>
2025-04-02 07:52:36 +0200

    ci-cd run1

--- index.js
+++ index.js
@@ -9,8 +9,11 @@ app.get('/', (req,res)=> {
 
 
 const PORT = process.env.PORT || 3009;
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  }
+
   
   export default app;
