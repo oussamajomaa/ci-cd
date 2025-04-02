1bf6a64c3c3219e9544b701492398ec9f1f043af
oussamajomaa <osm70@gmx.com>
2025-04-02 09:10:37 +0200

    ci-cd run1

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-const express = require('express')
+const express = require('express');
+const app = express();
 
-const app = express()
+app.get('/', (req, res) => {
+    res.json('Helo from CI/CD');
+  });
 
-app.get('/', (req,res)=> {
-    res.json('Helo from CI/CD')
-})
-
-const PORT = 4444
-if (require.main === module) {
-    console.log(require.main === module);
-    
+const PORT = process.env.PORT || 4444
+if (require.main === module) {    
     app.listen(PORT, () => {
       console.log(`Server running on http://localhost:${PORT}`);
     });
--- test/index.test.js
+++ test/index.test.js
@@ -1,15 +1,15 @@
-const request = require('supertest')
-const {expect} = require('chai')
+const request = require('supertest');
+const { expect } = require('chai');
+const app = require('../index');
 
-const app = require('../index')
-
-describe('La route de la racine',()=>{
-    it('doit retourner "helo from CI/CD"',(done)=> {
-        request(app).get('/')
-        .end((err,res)=>{
-            expect(res.status).to.equal(200)
-            expect(res.body).to.equal('Helo from CI/CD')
-            done()
-        })
-    })
-})
+describe('La route de la racine', () => {
+    it('doit retourner "Helo from CI/CD"', (done) => {
+        request(app)
+            .get('/')
+            .end((err, res) => {
+                expect(res.status).to.equal(200);
+                expect(res.body).to.equal('Helo from CI/CD');
+                done();
+            });
+    });
+});
